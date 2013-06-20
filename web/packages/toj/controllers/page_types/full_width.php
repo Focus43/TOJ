efe7a8b160ac166598c66488fe0450a72b8efc3e
Jonathan Hartman <jhartman86@gmail.com>
2013-06-20 12:52:49 -0600

    Updated full_width page type controller

--- web/packages/toj/controllers/page_types/full_width.php
+++ web/packages/toj/controllers/page_types/full_width.php
@@ -3,5 +3,18 @@
     class FullWidthPageTypeController extends TojPageController {
         
         protected $includeThemeAssets = true;
+
+        public function on_start(){
+            $this->set('backgroundImage', $this->getPageBackgroundImageURL());
+            parent::on_start();
+        }
+
+
+        protected function getPageBackgroundImageURL(){
+            $fileObj = $this->getCollectionObject()->getAttribute('page_background');
+            if( $fileObj instanceof File ){
+                return $fileObj->getRecentVersion()->getRelativePath();
+            }
+        }
         
     }
