f541929a585b6e99195fcf2a7296a9c9b4121d91
Jonathan Hartman <jhartman86@gmail.com>
2013-06-25 15:44:54 -0600

    Removed g translate really quick

--- web/packages/toj/elements/partials/site_settings.php
+++ web/packages/toj/elements/partials/site_settings.php
@@ -12,11 +12,11 @@
                     <a class="btn setFontSize" data-zoom="1.3" style="font-size:22px;">A</a>
                 </div>
                 <div class="span4">
-                    <div id="google_translate_element"></div><script type="text/javascript">
+                    <!--<div id="google_translate_element"></div><script type="text/javascript">
                         function googleTranslateElementInit() {
                             new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'de,es,fr,it,nl,no,ru', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
                         }
-                    </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+                    </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>-->
                 </div>
             </div>
         </div>
