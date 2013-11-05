453b0679c936da78bdc7db1532c541b23caceb6d
Jonathan Hartman <jhartman86@gmail.com>
2013-11-04 18:25:19 -0700

    FIle permissions preventing using a file set to filter for the right sidebar pictures when user wasn't logged in

--- web/packages/toj/elements/partials/sidebar_right.php
+++ web/packages/toj/elements/partials/sidebar_right.php
@@ -1,10 +1,11 @@
 <div class="inner">
     <?php //$a = new GlobalArea('Global Right'); $a->display($c);
         $fileList = new FileList;
+        $fileList->setPermissionLevel('view_file_set_file');
         $fileList->filterBySet( FileSet::getByName('Sidebar Photos') );
-        $files = $fileList->get(40);
+        $results = $fileList->get(40);
 
-        foreach($files AS $fileObj){ /** @var File $fileObj */ ?>
+        foreach($results AS $fileObj){ /** @var File $fileObj */ ?>
             <span class="thumb" style="background-image:url('<?php echo $fileObj->getApprovedVersion()->getRelativePath(); ?>');"></span>
         <?php } ?>
 </div>
