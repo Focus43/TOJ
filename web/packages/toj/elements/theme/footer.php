b9c3a2fe18bdcbeceb2e46d471d8e60a2ae4cc0d
Jonathan Hartman <jhartman86@gmail.com>
2013-11-06 21:58:03 -0700

    Aded links in the footer

--- web/packages/toj/elements/theme/footer.php
+++ web/packages/toj/elements/theme/footer.php
@@ -5,22 +5,22 @@
                 <div class="horiz">
                     <ul class="iconz">
                         <li class="showPopover" title="Socialize &amp; Engage" data-content="Share, explore, and learn more about the Town Of Jackson">
-                            <a>
+                            <a href="<?php echo View::url('socialize'); ?>">
                                 <i class="fa fa-facebook-square"></i><i class="fa fa-twitter-square"></i><i class="fa fa-rss-square"></i><span class="inner-text"><span class="hidden-xs">Socialize</span></span>
                             </a>
                         </li>
                         <li class="showPopover" title="Multi-Device Features" data-content="We've rebuilt the entire site from the ground up to support everyone from large desktops to on-the-go travelers. Learn more about the design features!">
-                            <a>
+                            <a href="<?php echo View::url('all-your-devices'); ?>">
                                 <i class="fa fa-desktop"></i><i class="fa fa-tablet"></i><i class="fa fa-mobile-phone"></i><span class="inner-text"><span class="hidden-xs">Multi-Device</span></span>
                             </a>
                         </li>
                         <li class="showPopover" title="Settings &amp; Accessibility" data-content="Control your experience with the Town Of Jackson site.">
-                            <a>
+                            <a href="<?php echo View::url('accessibility'); ?>">
                                 <i class="fa fa-cogs"></i><span class="inner-text"><span class="hidden-xs">Accessibility</span></span>
                             </a>
                         </li>
                         <li class="showPopover" title="Photos &amp; Media" data-content="Lorem ipsum dolor sit amet consect et tetur.">
-                            <a>
+                            <a href="<?php echo View::url('life-pictures'); ?>">
                                 <i class="fa fa-camera-retro"></i><span class="inner-text"><span class="hidden-xs">Photos</span></span>
                             </a>
                         </li>
