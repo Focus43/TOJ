a7ca2ac2f21de5a7b92a07dccdbc75c3124af9d5
Sam Fitz <abigwonderful@gmail.com>
2016-02-16 20:09:09 -0700

    update redis cache for toj to be unique

--- web/packages/concrete_redis/libraries/page_cache/type/redis.php
+++ web/packages/concrete_redis/libraries/page_cache/type/redis.php
@@ -2,11 +2,10 @@
 
 	class RedisPageCache extends Concrete5_Library_PageCache {
 		
-		const MD5_PEPPER 		= 'r3disPaGeCach3r',
-			  REDIS_HASH_HANDLE = 'redis_fp_cache';
+		const MD5_PEPPER = 'r3disPaGeCach3r';
 		
 		public function getRecord($mixed){
-			$result = ConcreteRedis::db()->hget(self::REDIS_HASH_HANDLE, $this->getCacheKey($mixed));
+			$result = ConcreteRedis::db()->hget(md5(BASE_URL), $this->getCacheKey($mixed));
 			if( !($result === null) ){
 				$record = @unserialize($result);
 				if( $record instanceof PageCacheRecord ){
@@ -16,27 +15,27 @@
 		}
 		
 		public function getCacheKey($mixed){
-			return md5( self::MD5_PEPPER . parent::getCacheKey($mixed) );
+			return md5( MD5_PEPPER . parent::getCacheKey($mixed) );
 		}
 		
 		
 		public function purgeByRecord(PageCacheRecord $rec){
-			ConcreteRedis::db()->hdel( self::REDIS_HASH_HANDLE, $this->getCacheKey($rec) );
+			ConcreteRedis::db()->hdel( md5(BASE_URL), $this->getCacheKey($rec) );
 		}
 		
 		public function flush(){
-			ConcreteRedis::db()->del( self::REDIS_HASH_HANDLE );
+			ConcreteRedis::db()->del( md5(BASE_URL) );
 		}
 		
 		public function purge(Page $c){
-			ConcreteRedis::db()->hdel( self::REDIS_HASH_HANDLE, $this->getCacheKey($c) );
+			ConcreteRedis::db()->hdel( md5(BASE_URL), $this->getCacheKey($c) );
 		}
 		
 		public function set(Page $c, $content){
 			$lifetime  = $c->getCollectionFullPageCachingLifetimeValue();
 			$cacheData = new PageCacheRecord($c, $content, $lifetime);
 			if( $content ){
-				ConcreteRedis::db()->hset( self::REDIS_HASH_HANDLE, $this->getCacheKey($c), serialize($cacheData) );
+				ConcreteRedis::db()->hset( md5(BASE_URL), $this->getCacheKey($c), serialize($cacheData) );
 			}
 		}
 		
