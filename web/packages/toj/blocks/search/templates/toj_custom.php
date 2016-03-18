936dab12ea62e324d147b68f85f52ed7b80c0af1
Arik Griffin <arik@focus-43.com>
2016-03-18 15:41:25 -0600

    copied date collection code from recent news php

--- web/packages/toj/blocks/search/templates/toj_custom.php
+++ web/packages/toj/blocks/search/templates/toj_custom.php
@@ -46,7 +46,7 @@
                             <li class="list-group-item">
                                 <h3><a href="<?=$r->getPath()?>" class="<?php echo (Collection::getByID($r->cID)->getVersionObject()->ctHandle === 'modal') ? 'modalize' : ''; ?>"><?=$r->getName()?></a></h3>
                                 <p>
-                                      <?php echo (Collection::getByID($r->cID)->getCollectionDatePublic('M d, Y'); ?><br/>
+                                      <?php echo $pageObj->getCollectionDatePublic('M d, Y'); ?><br/>
 
                                     <? if ($r->getDescription()) { ?>
                                         <?php  echo $this->controller->highlightedMarkup($tt->shortText($r->getDescription()),$query)?><br/>
